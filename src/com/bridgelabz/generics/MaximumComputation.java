f9a358c6cf1c646f2c52fd77b23e2169bc4b913a
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-17 00:06:05 +0530

    Added implemented program of finding max length string.

--- src/com/bridgelabz/generics/MaximumComputation.java
+++ src/com/bridgelabz/generics/MaximumComputation.java
@@ -1,6 +1,10 @@
 package com.bridgelabz.generics;
 
+import java.util.Scanner;
+
 public class MaximumComputation {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void findMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
         Integer maximumNumber;
 
@@ -33,6 +37,17 @@ public class MaximumComputation {
 
         System.out.println("The maximum number is: " + maximumNumber);
     }
+    public static String testStringMaximum(String x, String y, String z) {
+        String max=x;
+        if (y.compareTo(max) > 0) {
+            max=y;
+        }
+        if (z.compareTo(max)>0) {
+            max=z;
+        }
+        return max;
+
+    }
 
     public static void main(String[] args) {
 
@@ -43,6 +58,17 @@ public class MaximumComputation {
 
         Float number1 = 12f, number2 = 8f, number3 = 7f;
         findMaximum(number1, number2, number3);
+
+        String[] arr3 = new String[3];
+        System.out.println("enter the first String");
+        arr3[0]=scanner.next();
+        System.out.println("enter the second String");
+        arr3[1]=scanner.next();
+        System.out.println("enter the second String");
+        arr3[2]=scanner.next();
+
+        String max2 = testStringMaximum(arr3[0], arr3[1], arr3[2]);
+        System.out.println("maximum of three integers " + arr3[0] + " " + arr3[1] + " " + arr3[2] + " is " + max2);
 
     }
 
