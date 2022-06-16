b7efcb56cd09ca5bd12c7b90da93f85e1e1c5625
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-16 23:41:53 +0530

    Added implemented program of finding float type maximum number.

--- src/com/bridgelabz/generics/MaximumComputation.java
+++ src/com/bridgelabz/generics/MaximumComputation.java
@@ -18,6 +18,22 @@ public class MaximumComputation {
     }
 
 
+    public static void findMaximum(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        Float maximumNumber;
+
+        if(firstNumber.compareTo(secondNumber)>0 && firstNumber.compareTo(thirdNumber)>0) {
+            maximumNumber = firstNumber;
+        }
+        else if(secondNumber.compareTo(firstNumber)>0 && secondNumber.compareTo(thirdNumber)>0) {
+            maximumNumber = secondNumber;
+        }
+        else {
+            maximumNumber = thirdNumber;
+        }
+
+        System.out.println("The maximum number is: " + maximumNumber);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("----- Welcome To Computing Maximum Of Three Numbers Using Java Generics ------");
@@ -25,6 +41,10 @@ public class MaximumComputation {
         Integer firstNumber = 15, secondNumber = 20, thirdNumber = 25;
         findMaximum(firstNumber, secondNumber, thirdNumber);
 
-    }
-}
+        Float number1 = 12f, number2 = 8f, number3 = 7f;
+        findMaximum(number1, number2, number3);
 
+    }
+
+
+}
